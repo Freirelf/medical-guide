d4184beb8511be4b88d147680158dca7f1f80ce6
Lucas Freire <lucasfreirel.dev@gmail.com>
2024-10-17 12:06:12 -0300

    implement config appwrite and create user

---
+++ lib/actions/patient.actions.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { ID, Query } from "node-appwrite";
+import { users } from "../appwrite.config";
+import { parseStringify } from "../utils";
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    const newuser = await users.create(
+      ID.unique(),
+      user.email,
+      user.phone,
+      undefined,
+      user.name
+    );
+
+    return parseStringify(newuser);
+  } catch (error: any) {
+    console.error("An error occurred:", JSON.stringify(error, null, 2));  // imprimir o erro completo
+    if (error?.code === 409) {
+      const existingUser = await users.list([
+        Query.equal("email", [user.email]),
+      ]);
+
+      return existingUser.users[0];
+    }
+    console.error("An error occurred while creating a new user:", error);
+  }
+};
